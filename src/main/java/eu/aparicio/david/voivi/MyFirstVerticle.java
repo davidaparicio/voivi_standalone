45115b3d0f972a23f4cd7f59f3dc3539754616ba
David Aparicio <david.aparicio@free.fr>
2016-07-18 18:02:37 +0200

    Refactoring code about the mongoClient

--- src/main/java/eu/aparicio/david/voivi/MyFirstVerticle.java
+++ src/main/java/eu/aparicio/david/voivi/MyFirstVerticle.java
@@ -25,29 +25,25 @@ public class MyFirstVerticle extends AbstractVerticle {
     // Store our product
     // private Map<Integer, Feedback> feedbacks = new LinkedHashMap<>();
 
-    private JsonObject config;
-    private MongoClient client;
+    private JsonObject mongoConfig;
+    private MongoClient mongoClient;
 
     public static final String COLLECTION = "feedbacks";
 
     // Create some product
     private void createSomeData(Handler<AsyncResult<Void>> next, Future<Void> fut) {
-        Feedback mine = new Feedback("This restaurant was my best experience in my life !!", 18., "This restaurant", "be", "best experience", "3592c1ef-0df2-4e59-8302-d5c310743fce");
-        //System.out.println(Json.encodePrettily(mine));
-        //feedbacks.put(mine.getId(), mine);
-        Feedback omar = new Feedback("I found a hair on my plate. Yuck!!", 7.25, "I", "find", "hair", "df86f144-314c-4c13-842c-9208fcdb1972");
-        //feedbacks.put(omar.getId(), omar);
-        //System.out.println(Json.encodePrettily(omar));
         // Do we have data in the collection ?
-        client.count(COLLECTION, new JsonObject(), count -> {
+        mongoClient.count(COLLECTION, new JsonObject(), count -> {
             if (count.succeeded()) {
                 if (count.result() == 0) {
+                    Feedback mine = new Feedback("This restaurant was my best experience in my life !!", 18., "This restaurant", "be", "best experience", "3592c1ef-0df2-4e59-8302-d5c310743fce");
+                    Feedback omar = new Feedback("I found a hair on my plate. Yuck!!", 7.25, "I", "find", "hair", "df86f144-314c-4c13-842c-9208fcdb1972");
                     // no whiskies, insert data
-                    client.insert(COLLECTION, new JsonObject(Json.encodePrettily(mine)), ar -> {
+                    mongoClient.insert(COLLECTION, new JsonObject(Json.encodePrettily(mine)), ar -> {
                         if (ar.failed()) {
                             fut.fail(ar.cause());
                         } else {
-                            client.insert(COLLECTION, new JsonObject(Json.encodePrettily(omar)), ar2 -> {
+                            mongoClient.insert(COLLECTION, new JsonObject(Json.encodePrettily(omar)), ar2 -> {
                                 if (ar2.failed()) {
                                     fut.failed();
                                 } else {
@@ -69,11 +65,14 @@ public class MyFirstVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> fut) {
-        config = new JsonObject()
-                .put("connection_string", "mongodb://localhost:27017")
-                .put("db_name", "voivi");
+        mongoConfig = new JsonObject()
+                .put("connection_string",
+                        "mongodb://" +
+                        config().getString("mongo.ip", "localhost")+ ":" +
+                        config().getInteger("mongo.port", 27017))
+                .put("db_name", config().getString("db_name", "voivi"));
 
-        client = MongoClient.createShared(vertx,config);
+        mongoClient = MongoClient.createShared(vertx,mongoConfig);
 
         createSomeData(
             (nothing) -> startWebApp(
@@ -112,7 +111,7 @@ public class MyFirstVerticle extends AbstractVerticle {
     }
 
     private void getAll(RoutingContext routingContext) {
-        client.find(COLLECTION, new JsonObject(), results -> {
+        mongoClient.find(COLLECTION, new JsonObject(), results -> {
             List<JsonObject> objects = results.result();
             List<Feedback> feedbacks = objects.stream().map(Feedback::new).collect(Collectors.toList());
             routingContext.response()
@@ -124,7 +123,7 @@ public class MyFirstVerticle extends AbstractVerticle {
     private void addOne(RoutingContext routingContext) {
         final Feedback newFeedback = Json.decodeValue(routingContext.getBodyAsString(), Feedback.class);
 
-        client.insert(COLLECTION, new JsonObject(Json.encodePrettily(newFeedback)), r ->
+        mongoClient.insert(COLLECTION, new JsonObject(Json.encodePrettily(newFeedback)), r ->
             {
                 if (r.failed()) {
                     routingContext.response().setStatusCode(404).end();
@@ -143,7 +142,7 @@ public class MyFirstVerticle extends AbstractVerticle {
         if (id == null) {
             routingContext.response().setStatusCode(400).end();
         } else {
-            client.removeDocument(COLLECTION, new JsonObject().put("_id", id), res -> {
+            mongoClient.removeDocument(COLLECTION, new JsonObject().put("_id", id), res -> {
                 if (res.succeeded()) {
                     routingContext.response().setStatusCode(204).end();
                 } else {
